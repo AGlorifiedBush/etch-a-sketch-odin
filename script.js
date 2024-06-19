4bdd2b00e52f4bacd24103b7b1c5cb6f9d6a0e03
Matt Grych <james.grych@gmail.com>
2024-06-19 14:15:39 -0500

    Updates for the Etch-a-Sketch
    -Added hover effect whne mouse moves across the boxes
    
    -Grid is created with button at top
    
    -Can't seem to get the while loop to prevent the rows/columns from going over 100
    
    -Input is recieved but it doesn't seem to pass through the function creating the Grid
    
    -Grid is still produced as long at the numbers are below 100

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 const project = document.querySelector('.project');
 
-const grid = {rows:16, cols:16};
+let grid = {rows:0 , cols: 0} ;
 
 const totCells = grid.rows * grid.cols;
 
-createGrid(totCells)
+const changeBtn = document.getElementById("change");
+changeBtn.addEventListener("click", changeGrid);
+
+//createGrid(totCells);
 
 function createGrid(totCells) {
     for (let i = 0; i < totCells; i++) {
         const cell = document.createElement("div");
         project.append(cell);
+        cell.addEventListener("mouseenter", hoverColor);
+        cell.addEventListener("mouseleave", setToBox);
+        cell.classList.add("unclicked");
         
-        cell.classList.add("box"+`${i+1}`)
-        cell.setAttribute("id", "unclicked")
-        cell.addEventListener("click", colorChange)
+        cell.addEventListener("click", colorChange);
     }
-    project.style.setProperty(`grid-template-columns`, `repeat(${grid.cols}, 1fr)`)
+    project.style.setProperty(`grid-template-columns`, `repeat(${grid.cols}, 1fr)`);
 
 }
 
 function colorChange() {
-    const newCell = event.target
-    newCell.setAttribute("id", "clicked")
+    const newCell = event.target;
+    newCell.setAttribute("id", "clicked");
+    
+}
+
+function hoverColor() {
+    const hoverCell = event.target;
+    hoverCell.classList.add("hover");
+    hoverCell.classList.toggle("unclicked");
+}
+
+function setToBox() {
+    const cell = event.target;
+    cell.classList.remove("hover");
+    cell.classList.toggle("unclicked");
+}
+
+function changeGrid() {
+    const rows = parseInt(prompt("How many rows?"));
+    const cols = parseInt(prompt("How many columns?"));
+    while (rows > 100 ) {
+       rows = prompt("Too many rows... overloaded")
+    }
+    while (cols > 100) {
+        cols = prompt("Too many columns... oh noooo!")
+    }
+    grid.rows += rows;
+    grid.cols += cols;
+    
+    createGrid(totCells);
 }
